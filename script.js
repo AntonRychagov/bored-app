235b7dafaa82d4d6509f36ccd2100068cf0f5802
Anton Rychagov <ffhbioifcb@gmail.com>
2023-11-27 17:16:17 +0500

    fix bug

--- script.js
+++ script.js
@@ -2,29 +2,19 @@ const selectButtonNode = document.querySelector("#btnSelect");
 
 selectButtonNode.addEventListener("click", addActivity);
 
-const trueButton = () => {
-  selectButtonNode.disabled = true;
-  selectButtonNode.classList.add('loader')
-}
 
-const falseButton = () => {
-  selectButtonNode.disabled = false;
-  selectButtonNode.classList.remove('loader');
-}
 
 function addActivity() {
-  falseButton()
   fetch("https://www.boredapi.com/api/activity/")
-    .then((data) => data.json())
-    .then((result) => {
+    .then(function(response) {
+      document.querySelector('#subtitle').classList.add('loader');
+      return response.json()
+    })
+
+    .then(function(result) {
+      document.querySelector('#subtitle').classList.remove('loader');
       
       const activity = result.activity;
-      const status = result.ok;
-      if (result == status) {
-        document.querySelector('#subtitle').classList.add('loader')
-      }else {
-        document.querySelector('#subtitle').classList.remove('loader')
-      }
       
       document.querySelector("#subtitle").innerHTML = `
             ${activity}
