e4a6d5a807d139e944b401c0d63c02f388bc0135
Anton Rychagov <ffhbioifcb@gmail.com>
2023-11-27 16:21:19 +0500

    fix

--- script.js
+++ script.js
@@ -2,12 +2,30 @@ const selectButtonNode = document.querySelector("#btnSelect");
 
 selectButtonNode.addEventListener("click", addActivity);
 
+const trueButton = () => {
+  selectButtonNode.disabled = true;
+  selectButtonNode.classList.add('loader')
+}
+
+const falseButton = () => {
+  selectButtonNode.disabled = false;
+  selectButtonNode.classList.remove('loader');
+}
+
 function addActivity() {
+  falseButton()
   fetch("https://www.boredapi.com/api/activity/")
     .then((data) => data.json())
     .then((result) => {
+      
       const activity = result.activity;
-
+      const status = result.ok;
+      if (result == status) {
+        document.querySelector('#subtitle').classList.add('loader')
+      }else {
+        document.querySelector('#subtitle').classList.remove('loader')
+      }
+      
       document.querySelector("#subtitle").innerHTML = `
             ${activity}
             `;
@@ -16,5 +34,7 @@ function addActivity() {
             `;
 
       document.querySelector("body").classList.add("active");
+
+      
     });
 };
